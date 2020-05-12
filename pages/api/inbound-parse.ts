c89f345edaa4db1c51e0b75b9a0850d0f73c9949
Enrico Schaaf <enricoschaaf98@gmail.com>
2020-05-13 00:33:16 +0200

    inbound parse

--- pages/api/inbound-parse.ts
+++ pages/api/inbound-parse.ts
@@ -1,5 +1,19 @@
 import { NextApiRequest, NextApiResponse } from "next"
+import { IncomingForm } from "formidable"
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  console.log(JSON.parse(req.body))
+  const form = new IncomingForm()
+  form.parse(req, (_error, fields) => {
+    console.log(fields.from)
+    console.log(fields.to)
+    console.log(fields.subject)
+    console.log(fields.html)
+  })
+  res.end()
+}
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
 }
